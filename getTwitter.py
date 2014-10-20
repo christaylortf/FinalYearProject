d4b962c599a751db46e4dec2ead9828a3529c453
Chris Taylor <christaylortf@users.noreply.github.com>
2014-10-20 10:13:35 +0100

    Call html page the date and time of get request
    The html file that is outputted how has the current date and time as its name

--- getTwitter.py
+++ getTwitter.py
@@ -1,12 +1,16 @@
+import time
 import urllib2
 from BeautifulSoup import *
+# from bs4 import *
 
-print 'Welcome to the Get Twitter tool. This tool will allow you to download a page from Twitter to be used to extract the data'
 
-userResponse = raw_input("Please enter the full URL from the Tweet page")
+print 'Welcome to the Get Twitter tool. This tool will allow you to download a page from Twitter to be used to extract the data.'
+
+print "Current date & time {}".format(time.strftime("%c"))
+userResponse = raw_input("Please enter the full URL from the Tweet page: ")
 response = urllib2.urlopen(userResponse)
 html = response.read()
 
 soup = BeautifulSoup(html)
-with open('TwitterPage', 'a') as f:
+with open(time.strftime("%c"), 'a') as f:
 	f.write(html)
