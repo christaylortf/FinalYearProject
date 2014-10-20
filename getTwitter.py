fc8c949c58caaa012f166f1d0266e896f4ab0e3f
Chris Taylor <christaylortf@users.noreply.github.com>
2014-10-20 09:41:05 +0100

    Allow user to input url
    Allows the user to input a URL which will then be retrieved

--- getTwitter.py
+++ getTwitter.py
@@ -1,1 +1,7 @@
-print 'This script will be used to get the page and information from tiwtter'
+import urllib2
+
+print 'Welcome to the Get Twitter tool. This tool will allow you to download a page from Twitter to be used to extract the data'
+
+userResponse = raw_input("Please enter the full URL from the Tweet page")
+response = urllib2.urlopen(userResponse)
+html = response.read()
