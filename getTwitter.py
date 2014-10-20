a5b2b636a8b0b52689c53d0c7eddb348f3cda322
Chris Taylor <christaylortf@users.noreply.github.com>
2014-10-20 09:50:41 +0100

    Add BS
    Gets the html from the webpage and prints it to the screen

--- getTwitter.py
+++ getTwitter.py
@@ -1,7 +1,13 @@
 import urllib2
+from BeautifulSoup import *
 
 print 'Welcome to the Get Twitter tool. This tool will allow you to download a page from Twitter to be used to extract the data'
 
 userResponse = raw_input("Please enter the full URL from the Tweet page")
 response = urllib2.urlopen(userResponse)
 html = response.read()
+print 'debug 1'
+
+soup = BeautifulSoup(html)
+print 'debug 2'
+print(soup.prettify())
