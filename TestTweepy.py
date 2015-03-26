3a816d227650c843c95bded77546b4821090aa53
Chris Taylor <chris@crtaylor.co.uk>
2015-03-26 22:18:02 +0000

    Added auth to Tweepy

--- TestTweepy.py
+++ TestTweepy.py
@@ -1,10 +1,17 @@
 import tweepy
 
-auth = tweepy.OAuthHandler(yZA8ABXinV6rQlICV3DNyKwHV, D1xMDf3NnDCcYtu5KRcwes8MzQbAWW4pPQHEYL2tHH0T70L9E1)
+consumer_key = 'yZA8ABXinV6rQlICV3DNyKwHV'
+consumer_secret = 'D1xMDf3NnDCcYtu5KRcwes8MzQbAWW4pPQHEYL2tHH0T70L9E1'
+access_token = '1731498314-nPqsTsliicuwDmeBnZplnyM0ZYXR3TAnArZPhdR'
+access_token_secret = '1Tu4TYpL5ASLOzAFBDqWH721ETxVJUdT8hKnczSLyvWcH'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print tweet.text
+user = api.get_user('ChrisTaylor_TSA')
+print user.screen_name
+print user.followers_count
+for friend in user.friends():
+   print friend.screen_name
