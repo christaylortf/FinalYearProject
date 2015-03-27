84484e753a827170058f38d9a156051abcdf7114
Chris Taylor <chris@crtaylor.co.uk>
2015-03-27 01:44:10 +0000

    Added basic menu to control

--- control.py
+++ control.py
@@ -1,3 +1,19 @@
+from tweepyControl import *
+
 print "Welcome to the Twitter Forensic Storage Tool"
-tweetURL = raw_input("Enter the URL of the Tweet to investigate")
-print tweetURL
+tweetID = raw_input("Enter the ID of the Tweet to investigate")
+
+print '\n \n== Menu =='
+print '1. Enter tweet ID'
+if tweetID!="":
+	print '2. View Tweet Text'
+	print '3. View Tweet Author'
+
+menuChoice = input("Please choose an option: ")
+
+if menuChoice == 2:
+	print 'Showing Tweet Text'
+	showTweetText(tweetID)
+else:
+	print 'Oh oh!'
+
